e7aa0ea662784d834f70bc9db8c95416e60c145b
Shubhamrathhi <shubhamrathi2210@gmail.com>
2025-12-10 20:13:14 +0530

    Refactor GoUtils to use pipeline steps

--- src/org/example/utils/GoUtils.groovy
+++ src/org/example/utils/GoUtils.groovy
@@ -2,50 +2,73 @@ package org.example.utils
 
 class GoUtils implements Serializable {
 
-    static def installGoARM64() {
-        println "Installing Go 1.22.1 for ARM64..."
+    def steps  // Pipeline context
 
-        sh '''
-            wget https://go.dev/dl/go1.22.1.linux-arm64.tar.gz
-            sudo rm -rf /usr/local/go
-            sudo tar -C /usr/local -xzf go1.22.1.linux-arm64.tar.gz
-            export PATH=$PATH:/usr/local/go/bin
-            go version
-        '''
+    GoUtils(steps) {
+        this.steps = steps
     }
 
-    static def setupDependencies() {
-        println "Installing Go Modules (gorilla/mux, logrus)..."
-
-        sh '''
-            export PATH=$PATH:/usr/local/go/bin
-            go mod tidy
-            go get github.com/gorilla/mux
-            go get github.com/sirupsen/logrus
-            go list -m all
-        '''
+    void checkoutRepo(String repoUrl, String branch = 'main') {
+        steps.stage('Checkout') {
+            steps.echo "Cloning Employee API Repository..."
+            steps.git branch: branch, url: repoUrl
+        }
     }
 
-    static def installGoSecARM64() {
-        println "Installing GoSec ARM64 without Snap..."
-
-        sh '''
-            GSEC_VERSION=2.20.0
-            wget https://github.com/securego/gosec/releases/download/v${GSEC_VERSION}/gosec_${GSEC_VERSION}_linux_arm64.tar.gz
-            
-            tar -xzf gosec_${GSEC_VERSION}_linux_arm64.tar.gz
-            sudo mv gosec /usr/local/bin/gosec
-
-            gosec --version
-        '''
+    void installGoARM64() {
+        steps.stage('Install Go (ARM64)') {
+            steps.echo "Installing Go 1.22.1 for ARM64"
+            steps.sh '''
+                wget https://go.dev/dl/go1.22.1.linux-arm64.tar.gz
+                sudo rm -rf /usr/local/go
+                sudo tar -C /usr/local -xzf go1.22.1.linux-arm64.tar.gz
+                export PATH=$PATH:/usr/local/go/bin
+                go version
+            '''
+        }
     }
 
-    static def runGoSecScan() {
-        println "Running GoSec Scan..."
+    void setupDependencies() {
+        steps.stage('Setup Go Dependencies') {
+            steps.echo "Installing Go Modules (mux, logrus)"
+            steps.sh '''
+                export PATH=$PATH:/usr/local/go/bin
+                go mod tidy
+                go get github.com/gorilla/mux
+                go get github.com/sirupsen/logrus
+                go list -m all
+            '''
+        }
+    }
 
-        sh '''
-            export PATH=$PATH:/usr/local/go/bin
-            gosec ./... | tee gosec-report.txt
-        '''
+    void installGoSec() {
+        steps.stage('Install GoSec (ARM / AArch64)') {
+            steps.echo "Installing GoSec for ARM64 without Snap"
+            steps.sh '''
+                GSEC_VERSION=2.20.0
+                wget https://github.com/securego/gosec/releases/download/v${GSEC_VERSION}/gosec_${GSEC_VERSION}_linux_arm64.tar.gz
+                tar -xzf gosec_${GSEC_VERSION}_linux_arm64.tar.gz
+                sudo mv gosec /usr/local/bin/gosec
+                sudo chmod +x /usr/local/bin/gosec
+                gosec --version
+            '''
+        }
+    }
+
+    void runGoSecScan() {
+        steps.stage('Run GoSec Scan') {
+            steps.echo "Running GoSec Scan on ARM64"
+            steps.sh '''
+                export PATH=$PATH:/usr/local/go/bin
+                gosec ./... | tee gosec-report.txt
+            '''
+        }
+    }
+
+    void archiveReport() {
+        steps.stage('Archive Report') {
+            steps.echo "Archiving GoSec Scan Report"
+            steps.archiveArtifacts artifacts: 'gosec-report.txt', fingerprint: true
+        }
     }
 }
